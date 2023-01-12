08a484728554e3b7a111c0336f828ab3fa5e3293
spacetime <420169381@qq.com>
2023-01-12 15:42:02 +0800

    update sql

--- db_reggie.sql
+++ db_reggie.sql
@@ -48,19 +48,6 @@ CREATE TABLE `address_book` (
 INSERT INTO `address_book` VALUES ('1417414526093082626', '1417012167126876162', '小明', '1', '13812345678', null, null, null, null, null, null, '昌平区金燕龙办公楼', '公司', '1', '2021-07-20 17:22:12', '2021-07-20 17:26:33', '1417012167126876162', '1417012167126876162', '0');
 INSERT INTO `address_book` VALUES ('1417414926166769666', '1417012167126876162', '小李', '1', '13512345678', null, null, null, null, null, null, '测试', '家', '0', '2021-07-20 17:23:47', '2021-07-20 17:23:47', '1417012167126876162', '1417012167126876162', '0');
 
-
--- ----------------------------
--- Table structure for farmer
--- ----------------------------
-
-
-
-
-
-
-
-
-
 -- ----------------------------
 -- Table structure for category
 -- ----------------------------
@@ -391,5 +378,29 @@ CREATE TABLE `user` (
 -- Records of user
 -- ----------------------------
 
-INSERT INTO 'user' VALUES('1397849417888346113', '张三', '13271612180', '男', '412628202308156908', 'aaa.jpg', '1');
-INSERT INTO 'user' VALUES('1397849417888346114', '李四', '15690649180', '男', '412628202308156908', 'bbb.jpg', '1');
+INSERT INTO user VALUES('1397849417888346113', '张三', '13271612180', '男', '412628202308156908', 'aaa.jpg', '1');
+INSERT INTO user VALUES('1397849417888346114', '李四', '15690649180', '男', '412628202308156908', 'bbb.jpg', '1');
+
+
+-- ----------------------------
+-- Table structure for seller
+-- ----------------------------
+DROP TABLE IF EXISTS `seller`;
+CREATE TABLE `seller` (
+  `id` bigint(20) NOT NULL COMMENT '主键',
+  `name` varchar(50) COLLATE utf8_bin DEFAULT NULL COMMENT '姓名',
+  `password` varchar(64) COLLATE utf8_bin NOT NULL COMMENT '密码',
+  `phone` varchar(100) COLLATE utf8_bin NOT NULL COMMENT '手机号',
+  `sex` varchar(2) COLLATE utf8_bin DEFAULT NULL COMMENT '性别',
+  `id_number` varchar(18) COLLATE utf8_bin DEFAULT NULL COMMENT '身份证号',
+  `avatar` varchar(500) COLLATE utf8_bin DEFAULT NULL COMMENT '头像',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='用户信息';
+
+-- ----------------------------
+-- Records of seller
+-- ----------------------------
+
+INSERT INTO seller VALUES('1397849417888346113', '张三', 'e10adc3949ba59abbe56e057f20f883e', '13271612180', '男', '412628202308156908', 'aaa.jpg');
+INSERT INTO seller VALUES('1397849417888346114', '李四', 'e10adc3949ba59abbe56e057f20f883e', '15690649180', '男', '412628202308156908', 'bbb.jpg');
+
